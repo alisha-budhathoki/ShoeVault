f88784327a64ae6d5a102ef43fe76e82031815ea
Alisha <fmi-pcc-lt-25@FM-PC-LT-157s-MacBook-Pro.local>
2024-06-09 23:11:18 +0545

    Add Firestore dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shoe_vault/core/di/locator.dart';
 import 'package:shoe_vault/shoe_vault_app.dart';
 
-void main() {
+void main() async {
   setupLocator();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ShoeVaultApp());
 }
